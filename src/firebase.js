526bc84fc90cb10ca9d8cde3e5ea5c7690979499
dert1567 <35528731+dert1567@users.noreply.github.com>
2020-12-31 22:18:08 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import firebase from 'firebase'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCgU0qpTNl6fthpkWKXvJQs4HukuQbfJ-M",
-    authDomain: "linked-in-79ced.firebaseapp.com",
-    projectId: "linked-in-79ced",
-    storageBucket: "linked-in-79ced.appspot.com",
-    messagingSenderId: "894456874281",
-    appId: "1:894456874281:web:76afed271bc35b09a9e338"
+   //put your config please
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
